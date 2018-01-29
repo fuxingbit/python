d63f5867a886fe98df479a4080889707ee6e86c3
zjning95 <um7cpphy>
2018-01-29 22:36:37 +0800

    0129

---
+++ day4/diedaiqi.py
@@ -0,0 +1,33 @@
+# @Time    : 2018/1/29 20:59
+# @Author  : Alienzjn
+# @Email   : zjning95@126.com
+# @File    : diedaiqi.py
+
+#可以直接作用于for循环的对象统称为可迭代对象：Iterable。
+#可以被next()函数调用并不断返回下一个值的对象称为迭代器：Iterator。
+
+from collections import Iterable
+
+from collections import Iterator
+
+
+isinstance('abc',Iterable)
+isinstance((x for x in range(5)),Iterator)
+
+isinstance(iter('abc'),Iterator)
+
+#>>> isinstance('abc',Iterator)
+#False
+#>>> isinstance(iter('abc'),Iterator)
+#True
+
+# for x in [1,2,3,4,5]:
+#     pass
+
+it = iter([1,2,3,4,5])
+
+while True:
+    try:
+        x = next(it)
+    except StopIteration:
+        break
---
+++ day4/neizhihanshu.py
@@ -0,0 +1,31 @@
+# @Time    : 2018/1/29 22:21
+# @Author  : Alienzjn
+# @Email   : zjning95@126.com
+# @File    : neizhihanshu.py
+
+#abs()
+#取绝对值
+
+#all()
+#有0则false
+
+#any()
+#全空/0则false
+
+#print(ascii([1,2,"恺威和史蒂夫"]))
+
+#print(bin(255))
+#数字十进制转二进制
+
+#bool(1)
+#判断真假
+
+bytearray()
+#可修改的二进制字节格式
+
+a = bytes("abcde",encoding='utf-8')
+b = bytearray("abcde",encoding='utf-8')
+
+print( b[1] )
+
+#print(a.capitalize(),a)

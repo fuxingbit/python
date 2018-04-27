95139ea89302802b5d9ddb3b468006d8027e10cb
zjning <jianning.zhang@fuyinhy.com>
2018-04-27 17:40:23 +0800

    day5-atm

---
+++ day5/module_test/main.py
@@ -0,0 +1,27 @@
+# @Time    : 2018/4/27 16:58
+# @Author  : Alienzjn
+# @Email   : zjning95@126.com
+# @File    : main.py
+
+
+#import module_alex
+
+# print(module_alex.name)
+# module_alex.say()
+
+
+
+from module_alex import *
+
+from module_alex import logging as logging_alex
+
+
+print(name)
+say()
+
+def logging():
+    print("in the main")
+
+logging()
+
+logging_alex()
---
+++ day5/module_test/module_alex.py
@@ -0,0 +1,17 @@
+# @Time    : 2018/3/20 14:52
+# @Author  : Alienzjn
+# @Email   : zjning95@126.com
+# @File    : module_alex.py
+
+name = 'zjn'
+
+def say():
+    print("hello zjn")
+
+def logging():
+    print("in the alex")
+
+def running():
+    pass
+
+

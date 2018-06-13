f8914353f9448da865cccef51dbed7876c323cad
Jennings <zjn@wiwi.ink>
2018-06-13 20:35:22 +0800

    shutil

---
+++ day5/shutil_module.py
@@ -0,0 +1,23 @@
+# @Time    : 2018/6/1316:56
+# @Author  : Jennings
+# @Email   : zjn@wiwi.ink
+
+import shutil
+
+# f1 = open("shutil_test_file", encoding="utf-8")
+#
+# f2 = open("shutil_test_2", "w", encoding="utf-8")
+#
+# # 相当于把文件1复制到文件2
+# shutil.copyfileobj(f1, f2)
+
+# 只复制文件
+# shutil.copyfile("shutil_test_file", "shutil_test3")
+# 复制文件和权限
+# shutil.copy("shutil_test_file", "shutil_test3")
+# 递归拷贝，相当于拷贝目录
+# shutil.copytree("time_test", "time_test_new")
+# 删除
+# shutil.rmtree("time_test_new")
+# 压缩
+# shutil.make_archive("shutil_archive_test", "zip", "D:\work\python\day4")

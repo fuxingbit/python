25f8e853235e7d4cc47090076a29c3611cd432e1
Jennings <zjn@wiwi.ink>
2018-07-17 21:45:50 +0800

    day9

--- day10/io自动切换.py
+++ day10/io自动切换.py
@@ -6,19 +6,25 @@ import gevent
 
 
 def func1():
-	print('\033[31;1m李闯在跟海涛搞...\033[0m')
-	gevent.sleep(2)
-	print('\033[31;1m李闯又回去跟继续跟海涛搞...\033[0m')
+    print('\033[31;1m111111111111111111...\033[0m')
+    gevent.sleep(2)
+    print('\033[31;1m22222222222222...\033[0m')
 
 
 def func2():
-	print('\033[32;1m李闯切换到了跟海龙搞...\033[0m')
-	gevent.sleep(1)
-	print('\033[32;1m李闯搞完了海涛，回来继续跟海龙搞...\033[0m')
+    print('\033[32;1m3333333333333333...\033[0m')
+    gevent.sleep(1)
+    print('\033[32;1m44444444444444...\033[0m')
+
+
+def func3():
+    print('\033[33;1m55555555555555555...\033[0m')
+    gevent.sleep(0)
+    print('\033[33;1m66666666666666666666...\033[0m')
 
 
 gevent.joinall([
-	gevent.spawn(func1),
-	gevent.spawn(func2),
-	# gevent.spawn(func3),
+    gevent.spawn(func1),
+    gevent.spawn(func2),
+    gevent.spawn(func3),
 ])
